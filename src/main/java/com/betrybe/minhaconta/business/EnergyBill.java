e71a7930052645d76732cf8141a204081d5c90fa
esyamamoto <erika.s.yamamoto@gmail.com>
2024-04-03 18:51:45 -0300

    "req ate o 3 foi wee"

--- src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
+++ src/main/java/com/betrybe/minhaconta/business/EnergyBill.java
@@ -2,24 +2,44 @@ package com.betrybe.minhaconta.business;
 
 import com.ions.lightdealer.sdk.model.ElectronicDevice;
 
+import com.ions.lightdealer.sdk.model.Address;
+
 /**
  * The type Energy bill.
  */
 public class EnergyBill {
   // Req. 1 – Create class constructor and attributes.
+  Address address;
+  boolean residentialPlan;
+  double rate = 0.15;
+
+  public EnergyBill(Address address, boolean residentialPlan) {
+    this.address = address;
+    this.residentialPlan = residentialPlan;
+  }
 
   /**
    * Req. 2 – Calculates an adjusted tariff for non-residential plans.
    */
   public double adjustedTariff(double value) {
-    return 0d;
+
+    if (!residentialPlan) {
+      return value * 1.10;
+    }
+    return value;
   }
 
   /**
    * Req. 3 – Calculates the total usage of a collection of devices.
    */
   public static int calculateTotalUsage(ElectronicDevice[] devices) {
-    return 0;
+
+    double totalUsage = 0;
+
+    for (ElectronicDevice device : devices) {
+      totalUsage += Math.floor(device.monthlyKwh());
+    }
+    return (int) totalUsage;
   }
 
   /**
